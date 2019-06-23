0c7d28451d81c75cd389fb9985561718a1ea1a3a
Brett McDonald <bldmcdonald@gmail.com>
2019-06-23 20:15:10 +1200

    HandlingUserInput complete

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -8,14 +8,25 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @EnvironmentObject var userData: UserData
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
-            .navigationBarTitle(Text("Landmarks"), displayMode: .large)
+            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
@@ -23,11 +34,8 @@ struct LandmarkList: View {
 #if DEBUG
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"].identified(by: \.self)) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
+            .environmentObject(UserData())
     }
 }
 #endif
---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,26 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Brett McDonald on 23/06/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    let didChange = PassthroughSubject<UserData, Never>()
+    
+    var showFavoritesOnly = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    var landmarks = landmarkData {
+        didSet {
+            didChange.send(self)
+        }
+    }
+}
